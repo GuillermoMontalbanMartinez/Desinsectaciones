6f68314f7d348456ce7a596e07656e615954882f
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-08-10 16:39:13 +0200

    specification pattern client

---
+++ Desinsectaciones/Desinsectaciones/Clientes/Specifications/ISpecificationCliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Clientes.Specifications
+{
+    public interface ISpecificationCliente
+    {
+        public bool IsValid(ICliente cliente);
+    }
+}
---
+++ Desinsectaciones/Desinsectaciones/Clientes/Specifications/SpecificationCliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Clientes.Specifications
+{
+    public class SpecificationCliente : ISpecificationCliente
+    {
+        public bool IsValid(ICliente cliente)
+        {
+            if (cliente is null) return false;
+            if (string.IsNullOrEmpty(cliente.Nombre) || string.IsNullOrEmpty(cliente.Email) ||
+                string.IsNullOrEmpty(cliente.Telefono) || (cliente.CodigoPostal <= 0 || cliente.CodigoPostal > 9999))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
