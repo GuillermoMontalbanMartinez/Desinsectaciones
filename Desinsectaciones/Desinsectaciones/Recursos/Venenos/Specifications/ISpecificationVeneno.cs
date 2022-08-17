f49784519f23a9e921dd9c5ead58b28d9a3732c1
Guillermo <guillermosysti@gmail.com>
2022-08-17 18:15:43 +0200

    haciendo prueba desde nueva terminar subiendo el principio del patron specification del veneno

---
+++ Desinsectaciones/Desinsectaciones/Recursos/Venenos/Specifications/ISpecificationVeneno.cs
@@ -0,0 +1,14 @@
+﻿using Desinsectaciones.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Recursos.Venenos.Specifications
+{
+    public interface ISpecificationVeneno
+    {
+        public bool IsValid(IVeneno veneno);
+    }
+}
---
+++ Desinsectaciones/Desinsectaciones/Recursos/Venenos/Specifications/SpecificationVeneno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Recursos.Venenos.Specifications
+{
+    public class SpecificationVeneno : ISpecificationVeneno
+    {
+        public bool IsValid(IVeneno veneno)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
