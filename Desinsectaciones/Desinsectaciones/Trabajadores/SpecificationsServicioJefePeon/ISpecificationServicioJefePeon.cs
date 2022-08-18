f3b6bf0d08b9b468cffd6625fb8695f381dc796b
Guillermo <guillermosysti@gmail.com>
2022-08-18 16:24:22 +0200

    Specification de los servicios del jefe y los peones

---
+++ Desinsectaciones/Desinsectaciones/Trabajadores/SpecificationsServicioJefePeon/ISpecificationServicioJefePeon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Trabajadores.SpecificationsServicioJefePeon
+{
+    public interface ISpecificationServicioJefePeon
+    {
+        public bool IsValid(IJefeEquipoPeon trabajador);
+    }
+}
---
+++ Desinsectaciones/Desinsectaciones/Trabajadores/SpecificationsServicioJefePeon/SpecificationServicioJefePeon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desinsectaciones.Trabajadores.SpecificationsServicioJefePeon
+{
+    public class SpecificationServicioJefePeon : ISpecificationServicioJefePeon
+    {
+        public bool IsValid(IJefeEquipoPeon trabajador)
+        {
+            if (trabajador is not null)
+            {
+                return trabajador.IngresoPorServicio < 50 && trabajador.IngresoPorServicio > 0;
+            }
+
+            return false;
+        }
+    }
+}
